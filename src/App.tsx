a69fbaba4d950c7dcfaa51823970787867caf127
elda-mahaindra <faith030@gmail.com>
2021-11-08 05:59:13 +0700

    intro & setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+import "./App.css";
+
+const App: FunctionComponent = () => (
+  <div className="App">
+    <h1>Magic Match</h1>
+
+    <button>New Game</button>
+  </div>
+);
+
+export default App;
